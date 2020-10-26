733dea9cd380b022064a6d49e9441f69879c7fab
Nikita Danilov <danilov@gmail.com>
2020-10-26 13:00:27 +0300

    Ouptput descriptive prefix in describe(). Add various primes series.

--- series.go
+++ series.go
@@ -17,6 +17,145 @@ type Series interface {
 	Tail() Series
 }
 
+func main() {
+	z := &Zero{}
+	describe("0", z)
+	o := &One{}
+	describe("1", o)
+	x := &X{}
+	describe("x", x)
+	describe("6 + 6*x", &Scale{rat(6, 1), &Add{&One{}, &X{}}})
+	describe("(x + 1)/7", &Scale{rat(1, 7), &Add{&One{}, &X{}}})
+	describe("1/17 + x * (1/17 + x * (1/17 + ...", &Const{rat(1, 17)})
+	x2 := &Mul{x, x}
+	describe("x^2", x2)
+	mx := neg(x)
+	describe("-x", mx)
+	mo := neg(o)
+	describe("-1", mo)
+	/* 1 - x */
+	xm1 := sub(o, x)
+	xx := &Mul{&Add{o, x}, xm1}
+	describe("(1+x)*(1-x) == 1 - x^2", xx)
+	g := &Div{o, xm1}
+	describe("1/(1 - x) = 1 + x + x^2 + ...", g)
+	gg := &Div{o, &Mul{xm1, xm1}}
+	describe("1/((1 - x)^2) = 1 + 2*x + 3*x^2 + ...", gg)
+	fmt.Printf("%s .. %s\n",
+		eval(g, rat(1, 8)).FloatString(20), rat(8, 7).FloatString(20))
+	describe("(1 - y^2)(x^2) = 1 - x^4", &Subst{xx, x2})
+	describe("rev(x) = x", rev(x))
+	describe("d/dx(x^2) = 2*x", &Deriv{x2, 0})
+	describe("Int(x^2)*dx = x^3/3", &Integral{x2, 0})
+	mlnx := &Integral{g, 0}
+	describe("Int 1/(1 - x)*dx = -ln(1 - x) = x + x^2/2 + x^3/3 + ...", mlnx)
+	/*
+	 * Build a cyclic structure: expx = 1 + integral(expx)
+	 */
+	eint := &Integral{nil, 0}
+	expx := &Add{o, eint}
+	eint.f = expx
+	describe("exp", expx)
+	exp1 := eval(expx, rati(1))
+	fmt.Printf("%s %s\n", exp1, exp1.FloatString(20))
+	sinx := &Integral{nil, 0}
+	cosx := &Add{o, neg(&Integral{sinx, 0})}
+	sinx.f = cosx
+	describe("sin", sinx)
+	describe("cos", cosx)
+	for i := int64(0); i < 10; i++ {
+		fmt.Printf("%s\n", eval(&Add{&Mul{sinx, sinx}, &Mul{cosx, cosx}},
+			rati(i)))
+	}
+	/* rev(sinx) = arcsinx */
+	arcsinx := rev(sinx)
+	describe("arcsin", arcsinx)
+	fmt.Printf("%s\n", eval(arcsinx, rati(1)))
+	/* https://www.wolframalpha.com/input/?i=integrate+tan%28x%29+dx */
+	describe("Int tan(x) dx", &Integral{&Div{sinx, cosx}, 0})
+	describe(" .... ", &Deriv{&Integral{&Div{sinx, cosx}, 0}, 0})
+	describe(" .... ", &Div{sinx, cosx})
+	/* https://www.wolframalpha.com/input/?i=taylor+series+1+-+1%2F%281%2By%29 */
+	describe("rev(1/(1-x) - 1) = (1 - 1/(1+y))",
+		rev(&Add{&Div{o, sub(o, x)}, mo}))
+	/* https://www.wolframalpha.com/input/?i=taylor+series+ln%281%2Bx%29 */
+	lnx := rev(sub(expx, o))
+	describe("ln(1+x)", lnx)
+	/* https://www.wolframalpha.com/input/?i=integrate+exp%28-x%5E2%29 */
+	describe("Int(e^(-x^2))dx", &Integral{&Subst{expx, neg(x2)}, 0})
+	describe("sin(arcsin)", &Subst{sinx, arcsinx})
+	describe("arcsin(sin)", &Subst{arcsinx, sinx})
+	/* https://www.wolframalpha.com/input/?i=taylor+series+arcsin%28arcsin%28x%29%29 */
+	describe("arcsin(arcsin)", &Subst{arcsinx, arcsinx})
+	/* 3 + 1/10*(3 + 1/10 + ...) = 3 + 1/3 */
+	fmt.Printf("%s\n", eval(&Const{rat(3, 1)}, rat(1, 10)))
+	/* 9 + 1/10*(9 + 1/10 + ...) = 10 */
+	fmt.Printf("%s\n", eval(&Const{rat(9, 1)}, rat(1, 10)))
+	atan := rev(&Div{sinx, cosx})
+	/* \pi = 16 * atan(1/5) - 4 * atan(1/239) */
+	pix := sub(&Scale{rati(16), &Subst{atan, &Scale{rat(1, 5), x}}},
+		&Scale{rati(4), &Subst{atan, &Scale{rat(1, 239), x}}})
+	describe("pi", pix)
+	pi := eval(pix, rati(1))
+	fmt.Printf("%s %s\n", pi, pi.FloatString(40))
+	kprimes := &Prime{2, 0}
+	describe("k-primes", kprimes)
+	tprimes := &Map{0, func(n int64) *Rat {
+		if isprime(n) {
+			return rat(1, n)
+		} else {
+			return rati(0)
+		}
+	}}
+	describe("t-primes", tprimes)
+	for i := int64(-10); i <= int64(+10); i++ {
+		for _, j := range []int{10, 100, 1000} {
+			kp := ev(kprimes, rat(i, 10), j)
+			tp := ev(tprimes, rat(i, 10), j)
+			fmt.Printf("%2.2d %6.6d %s %s\n",
+				i, j, kp.FloatString(40), tp.FloatString(40))
+		}
+	}
+}
+
+func rat(n int64, d int64) *Rat {
+	return new(Rat).SetFrac64(n, d)
+}
+
+func rati(n int64) *Rat {
+	return rat(n, 1)
+}
+
+func ev(s Series, x *Rat, n int) *Rat {
+	var t Rat
+	var v Rat
+
+	t.SetInt64(1)
+	for i := 0; i < n; i++ {
+		var m Rat
+
+		m.Mul(&t, s.Head())
+		v.Add(&v, &m)
+		t.Mul(&t, x)
+		s = s.Tail()
+	}
+	return new(Rat).Set(&v)
+}
+
+func eval(s Series, x *Rat) *Rat {
+	return ev(s, x, 10)
+}
+
+func describe(prefix string, s Series) {
+	var i int
+	fmt.Printf("%s: [", prefix)
+	for i = 0; i < 10; i++ {
+		fmt.Printf("%s ", s.Head().RatString())
+		s = s.Tail()
+	}
+	fmt.Printf("... ]\n")
+}
+
 type Cons struct {
 	head *Rat
 	tail Series
@@ -28,135 +167,6 @@ func (self *Cons) Head() *Rat {
 
 func (self *Cons) Tail() Series {
 	return self.tail
-}
-
-func main() {
-	z := &Zero{}
-	describe(z)
-	o := &One{}
-	describe(o)
-	x := &X{}
-	describe(x)
-	/* 6 + 6*x */
-	describe(&Scale{rat(6, 1), &Add{&One{}, &X{}}})
-	/* (x + 1)/7 */
-	describe(&Scale{rat(1, 7), &Add{&One{}, &X{}}})
-	/* 1/17 + x * (1/17 + x * (1/17 + ... */
-	describe(&Const{rat(1, 17)})
-	x2 := &Mul{x, x} /* x^2 */
-	describe(x2)
-	/* -x */
-	mx := neg(x)
-	describe(mx)
-	/* -1 */
-	mo := neg(o)
-	describe(mo)
-	/* 1 - x */
-	xm1 := sub(o, x)
-	/* (1+x)*(1-x) == 1 - x^2 */
-	xx := &Mul{&Add{o, x}, xm1}
-	describe(xx)
-	/* 1/(1 - x) = 1 + x + x^2 + ... */
-	g := &Div{o, xm1}
-	describe(g)
-	/* 1/((1 - x)^2) = 1 + 2*x + 3*x^2 + ... */
-	gg := &Div{o, &Mul{xm1, xm1}}
-	describe(gg)
-	fmt.Printf("%s .. %s\n",
-		eval(g, rat(1, 8)).FloatString(20), rat(8, 7).FloatString(20))
-	/* (1 - y^2)(x^2) = 1 - x^4 */
-	describe(&Subst{xx, x2})
-	/* rev(x) = x */
-	describe(rev(x))
-	/* d/dx(x^2) = 2*x */
-	describe(&Deriv{x2, 0})
-	/* \Int(x^2)*dx = x^3/3 */
-	describe(&Integral{x2, 0})
-	/* \Int 1/(1 - x)*dx = -ln(1 - x) = x + x^2/2 + x^3/3 + ... */
-	mlnx := &Integral{g, 0}
-	describe(mlnx)
-	/*
-	 * Build a cyclic structure: expx = 1 + integral(expx)
-	 */
-	eint := &Integral{nil, 0}
-	expx := &Add{o, eint}
-	eint.f = expx
-	describe(expx)
-	exp1 := eval(expx, rati(1))
-	fmt.Printf("%s %s\n", exp1, exp1.FloatString(20))
-	sinx := &Integral{nil, 0}
-	cosx := &Add{o, neg(&Integral{sinx, 0})}
-	sinx.f = cosx
-	describe(sinx)
-	describe(cosx)
-	for i := int64(0); i < 10; i++ {
-		fmt.Printf("%s\n", eval(&Add{&Mul{sinx, sinx}, &Mul{cosx, cosx}},
-			rati(i)))
-	}
-	/* rev(sinx) = arcsinx */
-	arcsinx := rev(sinx)
-	describe(arcsinx)
-	fmt.Printf("%s\n", eval(arcsinx, rati(1)))
-	/* https://www.wolframalpha.com/input/?i=integrate+tan%28x%29+dx */
-	describe(&Integral{&Div{sinx, cosx}, 0})
-	describe(&Deriv{&Integral{&Div{sinx, cosx}, 0}, 0})
-	describe(&Div{sinx, cosx})
-	/* https://www.wolframalpha.com/input/?i=taylor+series+1+-+1%2F%281%2By%29 */
-	describe(rev(&Add{&Div{o, sub(o, x)}, mo}))
-	/* https://www.wolframalpha.com/input/?i=taylor+series+ln%281%2Bx%29 */
-	lnx := rev(sub(expx, o))
-	describe(lnx)
-	/* https://www.wolframalpha.com/input/?i=integrate+exp%28-x%5E2%29 */
-	describe(&Integral{&Subst{expx, neg(x2)}, 0})
-	describe(&Subst{sinx, arcsinx})
-	describe(&Subst{arcsinx, sinx})
-	/* https://www.wolframalpha.com/input/?i=taylor+series+arcsin%28arcsin%28x%29%29 */
-	describe(&Subst{arcsinx, arcsinx})
-	/* 3 + 1/10*(3 + 1/10 + ...) = 3 + 1/3 */
-	fmt.Printf("%s\n", eval(&Const{rat(3, 1)}, rat(1, 10)))
-	/* 9 + 1/10*(9 + 1/10 + ...) = 10 */
-	fmt.Printf("%s\n", eval(&Const{rat(9, 1)}, rat(1, 10)))
-	atan := rev(&Div{sinx, cosx})
-	/* \pi = 16 * atan(1/5) - 4 * atan(1/239) */
-	pix := sub(&Scale{rati(16), &Subst{atan, &Scale{rat(1, 5), x}}},
-		&Scale{rati(4), &Subst{atan, &Scale{rat(1, 239), x}}})
-	describe(pix)
-	pi := eval(pix, rati(1))
-	fmt.Printf("%s %s\n", pi, pi.FloatString(40))
-}
-
-func rat(n int64, d int64) *Rat {
-	return new(Rat).SetFrac64(n, d)
-}
-
-func rati(n int64) *Rat {
-	return rat(n, 1)
-}
-
-func eval(s Series, x *Rat) *Rat {
-	var t Rat
-	var v Rat
-
-	t.SetInt64(1)
-	for i := 0; i < 10; i++ {
-		var m Rat
-
-		m.Mul(&t, s.Head())
-		v.Add(&v, &m)
-		t.Mul(&t, x)
-		s = s.Tail()
-	}
-	return new(Rat).Set(&v)
-}
-
-func describe(s Series) {
-	var i int
-	fmt.Printf("[")
-	for i = 0; i < 10; i++ {
-		fmt.Printf("%s ", s.Head().RatString())
-		s = s.Tail()
-	}
-	fmt.Printf("... ]\n")
 }
 
 type (
@@ -311,4 +321,42 @@ func (self *Integral) Tail() Series {
 		ff = ff.Tail()
 	}
 	return &Integral{ff, self.n + 1}
+}
+
+type Prime struct {
+	p int64
+	n int64
+}
+
+func (self *Prime) Head() *Rat {
+	return rat(1, self.p)
+}
+
+func (self *Prime) Tail() Series {
+	p := self.p
+	n := self.n
+	for {
+		p++
+		n++
+		if isprime(p) {
+			return &Prime{p, n}
+		}
+	}
+}
+
+func isprime(p int64) bool {
+	return new(Int).SetInt64(p).ProbablyPrime(1)
+}
+
+type Map struct {
+	n   int64
+	val func(int64) *Rat
+}
+
+func (self *Map) Head() *Rat {
+	return self.val(self.n)
+}
+
+func (self *Map) Tail() Series {
+	return &Map{self.n + 1, self.val}
 }
